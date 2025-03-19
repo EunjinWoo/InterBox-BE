6a4050ccce0b2c9d69541c28bbba2dfc74b1d97d
EunjinWoo <esther0904@naver.com>
2025-03-19 12:47:59 +0900

    Setting: Initial 환경 세팅

---
+++ src/main/java/com/interbox/interbox_be/InterboxBeApplication.java
@@ -0,0 +1,13 @@
+package com.interbox.interbox_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterboxBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterboxBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interbox/interbox_be/InterboxBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interbox.interbox_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterboxBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
